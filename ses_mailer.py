e51a1dc4c6df6d65fd7d493662fc7a4d2496e0b5
Semyon Maryasin <simeon@maryasin.name>
2016-10-23 09:49:16 +0300

    Fix indentation

--- ses_mailer.py
+++ ses_mailer.py
@@ -126,8 +126,8 @@ class Mail(object):
         else:
             if region:
                 self.ses = boto.ses.connect_to_region(region,
-                                                        aws_access_key_id=aws_access_key_id,
-                                                        aws_secret_access_key=aws_secret_access_key)
+                                                      aws_access_key_id=aws_access_key_id,
+                                                      aws_secret_access_key=aws_secret_access_key)
             else:
                 self.ses = boto.connect_ses(aws_access_key_id=aws_access_key_id,
                                             aws_secret_access_key=aws_secret_access_key)
